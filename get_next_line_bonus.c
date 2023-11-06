17f9a77730df3ebf6c9d096615ee65f3a963d41c
Ota Watanabe <owatanab@c1r21s11.42tokyo.jp>
2023-11-06 18:03:57 +0900

    added bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: owatanab <owatanab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/31 14:17:50 by otawatanabe       #+#    #+#             */
-/*   Updated: 2023/11/06 17:42:00 by owatanab         ###   ########.fr       */
+/*   Created: 2023/11/06 17:51:00 by owatanab          #+#    #+#             */
+/*   Updated: 2023/11/06 17:58:40 by owatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,21 @@ char	*get_next_line(int fd)
 {
 	char		*buf;
 	char		*line;
-	static char	*rest;
+	static char	*rest[1024];
 
 	line = NULL;
-	if (BUFFER_SIZE <= 0
-		|| (rest && *rest && check_new_line(&rest, &line, -1) == 0))
+	if (BUFFER_SIZE <= 0 || (rest[fd]
+			&& *(rest[fd]) && check_new_line(&rest[fd], &line, -1) == 0))
 		return (line);
-	free(rest);
-	rest = NULL;
+	free(rest[fd]);
+	rest[fd] = NULL;
 	buf = malloc(BUFFER_SIZE + 1);
 	if (buf == NULL)
 	{
 		free(line);
 		return (NULL);
 	}
-	line = concat(line, buf_read(&buf, fd, &rest), -1);
+	line = concat(line, buf_read(&buf, fd, &rest[fd]), -1);
 	free(buf);
 	return (line);
 }
---
+++ get_next_line_bonus.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: owatanab <owatanab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 17:51:45 by owatanab          #+#    #+#             */
+/*   Updated: 2023/11/06 17:54:38 by owatanab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
